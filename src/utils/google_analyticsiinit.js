36398f8be32cd7d51d7320b11c6f85af44b63f2f
Yuvraj Singh <98007961+SinghYuvraj0506@users.noreply.github.com>
2022-09-30 00:39:40 +0530

    Delete src directory

--- src/utils/google_analyticsiinit.js
+++
@@ -1,8 +0,0 @@
-import ReactGA from "react-ga"
-
-const googleAnalyticsAction = async() =>{
-    ReactGA.initialize("G-LCHM48D5F0")
-    ReactGA.pageview(window.location.pathname + window.location.search)
-}
-
-export default googleAnalyticsAction;
